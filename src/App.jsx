63849ea2eb7618eabf4192edbb088f80b1ef9068
Akash Chhabria <akash.chhabria@ryerson.ca>
2019-10-16 19:14:58 -0400

    created a skeleton for the toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import Home from "./Component/Home/Home";
+import Toolbar from "./Component/Toolbar/Toolbar";
 
 class App extends Component {
   render() {
       return (
           <div className="App">
+              <Toolbar/>
               <Home />
           </div>
       );
